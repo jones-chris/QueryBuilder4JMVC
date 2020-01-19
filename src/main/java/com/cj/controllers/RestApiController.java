35f799095ca1bab238f4d7c306d2295fa87bb4f3
Chris Jones <chris.jones@cfacorp.com>
2020-01-19 18:04:13 -0500

    feat:  converted from gradle to maven

--- src/main/java/com/cj/controllers/RestApiController.java
+++ src/main/java/com/cj/controllers/RestApiController.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class RestApiController {
     private LoggingService loggingService;
