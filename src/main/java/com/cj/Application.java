f6d5c4bff718e62f9c792caacc98af6f61d61256
Chris Jones <chris.jones@cfacorp.com>
2020-02-03 21:17:21 -0500

    Refactored java code and added S3 to pom to get querybuilder4j library

--- src/main/java/com/cj/Application.java
+++ src/main/java/com/cj/Application.java
@@ -1,6 +1,5 @@
 package com.cj;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        //Set up timed task to send log records to S3 bucket at set interval.
-//        Date date = new Date();
-//        Timer timer = new Timer();
-//        timer.schedule(new S3TransferWrapper(), date, Constants.s3TimedTaskInterval);
-
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-
         SpringApplication.run(Application.class, args);
     }
 
--- src/main/java/com/cj/controllers/RestApiController.java
+++ src/main/java/com/cj/controllers/RestApiController.java
@@ -1,7 +1,5 @@
 package com.cj.controllers;
 
-import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.model.PublishRequest;
 import com.cj.config.Constants;
 import com.cj.service.*;
 import com.cj.utils.Converter;
@@ -135,7 +133,7 @@ public class RestApiController {
      * @return
      */
     @GetMapping(value = "/tablesAndViews/{schema}")
-    public ResponseEntity<String> getTablesAndViews(@PathVariable(value = "schema", required = true) String schema) {
+    public ResponseEntity<String> getTablesAndViews(@PathVariable(value = "schema") String schema) {
         try {
             schema = (schema.equals("null")) ? null : schema;
             String tablesJson = databaseMetaDataService.getTablesAndViews(schema);
