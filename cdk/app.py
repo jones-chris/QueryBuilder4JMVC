cb07d2e3ec1ab94d2fcd92277b7c10c0a218206e
Chris Jones <chris.jones@cfacorp.com>
2020-03-01 08:29:12 -0500

    fix:  Updated buildspec to include maven settings change

--- cdk/app.py
+++ cdk/app.py
@@ -6,10 +6,24 @@ from aws_cdk import core
 from cdk.cdk_stack import Qb4jStack
 
 
+first_arg = sys.argv[1].split('=')
+if len(first_arg) == 2:
+    first_arg_name = first_arg[0]
+    first_arg_value = first_arg[1]
+    if first_arg_name == 'env':
+        env = first_arg_value
+    else:
+        raise ValueError('env is the expected first argument, like env=dev')
+else:
+    raise ValueError('env is the expected first argument, like env=dev')
+
+print(sys.argv)
+print(env)
+
 app = core.App(outdir='cdk.out')
 
-env = app.node.try_get_context('env')
-print(env)
+# env = app.node.try_get_context('env')
+# print(env)
 
 Qb4jStack(app, "QB4J", env=env)
 
--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -7,25 +7,27 @@ class Qb4jStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # Codebuild
-        codebuild_project = aws_codebuild.Project(self, 'CodeBuildProject',
-                                                  project_name='QueryBuilder4JMVC',
-                                                  source=aws_codebuild.Source.git_hub(
-                                                      owner='jones-chris',
-                                                      repo='QueryBuilder4JMVC',
-                                                      branch_or_ref='master',
-                                                      clone_depth=1),
-                                                  badge=True,
-                                                  artifacts=aws_codebuild.Artifacts.s3(
-                                                      bucket=aws_s3.Bucket(self, 'CodeBuildArtifactBucket',
-                                                                           bucket_name='qb4j-mvc',
-                                                                           block_public_access=aws_s3.BlockPublicAccess(
-                                                                               restrict_public_buckets=True
-                                                                           )),
-                                                      name=f'querybuilder4jmvc-{env}',
-                                                      include_build_id=True,
-                                                      path='build/')
-                                                  # allow_all_outbound=True  # True so docker image can be sent to docker hub.
-                                                  )
+        codebuild_project = aws_codebuild.Project(
+            self, 'CodeBuildProject',
+            project_name='QueryBuilder4JMVC',
+            source=aws_codebuild.Source.git_hub(
+                owner='jones-chris',
+                repo='QueryBuilder4JMVC',
+                branch_or_ref='master',
+                clone_depth=1),
+            badge=True,
+            artifacts=aws_codebuild.Artifacts.s3(
+                bucket=aws_s3.Bucket(self, 'CodeBuildArtifactBucket',
+                                     bucket_name='qb4j-mvc',
+                                     block_public_access=aws_s3.BlockPublicAccess(
+                                         restrict_public_buckets=True
+                                     )),
+                name=f'querybuilder4jmvc-{env}',
+                include_build_id=True,
+                path='build/'),
+            environment=aws_codebuild.BuildEnvironment(privileged=True)  # This allows codebuild to build docker images.
+            # allow_all_outbound=True  # True so docker image can be sent to docker hub.
+        )
 
         # ECS
 
