fe2f104d0e9504a4b090223167cfa5eac64301e0
Chris Jones <chris.jones@cfacorp.com>
2020-03-01 16:56:49 -0500

    fix:  Successfully built jar and pushed docker image to docker hub

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -7,29 +7,31 @@ class Qb4jStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # Codebuild project.
-        codebuild_project = aws_codebuild.Project(self, 'CodeBuildProject',
-                                                  project_name='QueryBuilder4JMVC',
-                                                  badge=True,
-                                                  source=aws_codebuild.Source.git_hub(
-                                                      owner='jones-chris',
-                                                      repo='QueryBuilder4JMVC',
-                                                      branch_or_ref='master',
-                                                      clone_depth=1),
-                                                  artifacts=aws_codebuild.Artifacts.s3(
-                                                      bucket=aws_s3.Bucket(self, 'CodeBuildArtifactBucket',
-                                                                           bucket_name='qb4j-mvc',
-                                                                           block_public_access=aws_s3.BlockPublicAccess(
-                                                                               restrict_public_buckets=True,
-                                                                               block_public_policy=True
-                                                                           )),
-                                                      name=f'querybuilder4jmvc-{env}.jar',
-                                                      include_build_id=True,
-                                                      path='build/'),
-                                                  environment=aws_codebuild.BuildEnvironment(
-                                                      privileged=True  # This allows codebuild to build docker images.
-                                                  )
-                                                  # allow_all_outbound=True  # True so docker image can be sent to docker hub.
-                                                  )
+        codebuild_project = aws_codebuild.Project(
+            self, 'CodeBuildProject',
+            project_name='QueryBuilder4JMVC',
+            badge=True,
+            source=aws_codebuild.Source.git_hub(
+                owner='jones-chris',
+                repo='QueryBuilder4JMVC',
+                branch_or_ref='master',
+                clone_depth=1),
+            artifacts=aws_codebuild.Artifacts.s3(
+                bucket=aws_s3.Bucket(
+                    self, 'CodeBuildArtifactBucket',
+                    bucket_name='qb4j-mvc',
+                    block_public_access=aws_s3.BlockPublicAccess(
+                        restrict_public_buckets=True,
+                        block_public_policy=True
+                    )),
+                name=f'querybuilder4jmvc-{env}.jar',
+                include_build_id=True,
+                path='build/'),
+            environment=aws_codebuild.BuildEnvironment(
+                privileged=True  # This allows codebuild access to docker in order to build docker images.
+            )
+            # allow_all_outbound=True  # True so docker image can be sent to docker hub.
+        )
 
         # Add permissions to S3 bucket that contains maven settings.xml.
         codebuild_project.role.add_to_policy(aws_iam.PolicyStatement(
@@ -45,6 +47,18 @@ class Qb4jStack(core.Stack):
             ]
         ))
 
+        # Add permissions for SSM so they can be pulled in during the CodeBuild builds.
+        codebuild_project.role.add_to_policy(aws_iam.PolicyStatement(
+            effect=aws_iam.Effect.ALLOW,
+            actions=[
+                'ssm:GetParameters'
+            ],
+            resources=[
+                '*'
+            ]
+        ))
+
         # ECS
+        
 
         # Define CW alarm with SNS topic?
