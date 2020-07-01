e9ce8dec59f382c1ee45498a4c331ad393c63d3f
Chris Jones <chris.jones@cfacorp.com>
2020-06-30 22:14:38 -0400

    Minor changes to response body of /metadata/database endpoint.  Changed version.

--- src/main/java/com/cj/controllers/RestApiController.java
+++ src/main/java/com/cj/controllers/RestApiController.java
@@ -8,13 +8,9 @@ import com.cj.service.database.healer.DatabaseHealerService;
 import com.cj.service.database.metadata.DatabaseMetaDataService;
 import com.cj.service.querytemplate.QueryTemplateService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.querybuilder4j.databasemetadata.QueryTemplateDao;
 import com.querybuilder4j.statements.SelectStatement;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,15 +20,12 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
 @CrossOrigin(origins = { "http://localhost:3000", "http://querybuilder4j.net" })
 @RestController
 public class RestApiController {
-    private DatabaseAuditService databaseAuditService;
-    private DatabaseHealerService databaseHealerService;
     private DatabaseMetaDataService databaseMetaDataService;
     private QueryTemplateService queryTemplateService;
     private QueryTemplateDao queryTemplateDao;  // todo:  can this be a prebuilt class from qb4j and connection properties are just passed or injected into it?
@@ -40,15 +33,11 @@ public class RestApiController {
     private Qb4jConfig qb4jConfig;
 
     @Autowired
-    public RestApiController(DatabaseAuditService databaseAuditService,
-                             DatabaseHealerService databaseHealerService,
-                             DatabaseMetaDataService databaseMetaDataService,
+    public RestApiController(DatabaseMetaDataService databaseMetaDataService,
                              QueryTemplateService queryTemplateService,
                              QueryTemplateDao queryTemplateDao,
                              DatabaseDataService databaseDataService,
                              Qb4jConfig qb4jConfig) {
-        this.databaseAuditService = databaseAuditService;
-        this.databaseHealerService = databaseHealerService;
         this.databaseMetaDataService = databaseMetaDataService;
         this.queryTemplateService = queryTemplateService;
         this.queryTemplateDao = queryTemplateDao;
@@ -60,9 +49,9 @@ public class RestApiController {
      *
      */
     @GetMapping(value = "/metadata/database")
-    public ResponseEntity<List<Database>> getDatabases() throws JsonProcessingException {
+    public ResponseEntity<List<Database>> getDatabases() {
         List<Database> databases = qb4jConfig.getTargetDataSources().stream()
-                .map(targetDataSource -> new Database(targetDataSource.getName()))
+                .map(targetDataSource -> new Database(targetDataSource.getName(), targetDataSource.getDatabaseType()))
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(databases);
@@ -78,8 +67,8 @@ public class RestApiController {
      */
     @GetMapping(value = "/query-template")
     public ResponseEntity<List<String>> getQueryTemplates(@RequestParam(required = false) Integer limit,
-                                                    @RequestParam(required = false) Integer offset,
-                                                    @RequestParam(required = false) boolean ascending) throws Exception {
+                                                          @RequestParam(required = false) Integer offset,
+                                                          @RequestParam(required = false) boolean ascending) throws Exception {
         List<String> queryTemplateNames = queryTemplateService.getNames(limit, offset, ascending);
         return ResponseEntity.ok(queryTemplateNames);
     }
@@ -103,7 +92,7 @@ public class RestApiController {
      * @return
      */
     @PostMapping(value = "/query-template")
-    public ResponseEntity<String> saveQueryTemplate(@RequestBody SelectStatement selectStatement) {
+    public ResponseEntity<?> saveQueryTemplate(@RequestBody SelectStatement selectStatement) {
         if (selectStatement.getName() == null) {
             throw new RuntimeException("The name of the select statement cannot be null when saving the statement " +
                     "as a query template");
@@ -114,7 +103,7 @@ public class RestApiController {
 
         queryTemplateService.save(selectStatement.getName(), json);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
     /**
@@ -204,124 +193,19 @@ public class RestApiController {
      */
     @PostMapping(value = "/data/{database}/query")
     public ResponseEntity<QueryResult> getQueryResults(@PathVariable String database,
-                                                  @RequestBody SelectStatement selectStatement) throws Exception {
+                                                       @RequestBody SelectStatement selectStatement) throws Exception {
         selectStatement.setQueryTemplateDao(queryTemplateDao);
         Properties properties = this.qb4jConfig.getTargetDataSource(database).getProperties();
         String sql = selectStatement.toSql(properties);
         QueryResult queryResult = databaseDataService.executeQuery(database, sql);
 
-        return new ResponseEntity<>(queryResult, HttpStatus.OK);
+        return ResponseEntity.ok(queryResult);
     }
-
-//    @PostMapping("/{database}/tableau-wdc-types")
-//    public ResponseEntity<String> getColumnDataTypes(@PathVariable String database,
-//                                                     @RequestBody TableauColumns columns) throws SQLException {
-//        List<String> tables = new ArrayList<>();
-//        columns.getColumns().forEach((column) -> {
-//            String table = column.split("\\.")[0];
-//            if (! tables.contains(table)) { tables.add(table); }
-//        });
-//
-//        List<Column> allColumns = new ArrayList<>();
-//        for (String table : tables) {
-//            List<Column> tableMetaData = databaseMetaDataService.getColumns(database,null, table);
-//            allColumns.addAll(tableMetaData);
-//        }
-//
-//        List<TableauColumnSchema> columnSchemas = new ArrayList<>();
-//        for (String column : columns.getColumns()) {
-//            Integer sqlType = sqlTypes.get(column);
-//            String tableType = Constants.tableauDataTypeMappings.get(sqlType);
-//            columnSchemas.add(new TableauColumnSchema(column.split("\\.")[1], tableType));
-//        }
-//        TableauTableSchema tableauTableSchema = new TableauTableSchema("qb4j", "my alias", columnSchemas);
-//
-//        return ResponseEntity.ok(new Gson().toJson(tableauTableSchema));
-//    }
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<String> handleException(HttpServletRequest request, Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<?> handleException(HttpServletRequest request, Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new HashMap<>().put("message", ex.getMessage()));
     }
 
-    private JSONObject getDatabaseAuditResults(String databaseName, SelectStatement selectStatement, String sql) {
-//      Log the SelectStatement and the database audit results to logging.db
-        Map<String, Boolean> databaseAuditResults = databaseAuditService.runAllChecks(databaseName);
-
-//      Set QueryTemplateDao to null before persisting SelectStatement.
-        selectStatement.setQueryTemplateDao(null);
-
-        Map<String, Runnable> healerFunctions;
-        if (databaseAuditResults.values().contains(false)) {
-            // Publish message to SNS topic to alert interested parties.
-//                try {
-//                    publishSnsMessage();
-//                } catch (Exception ex) {
-//                    // Todo:  Log the exception and SelectStatement.toString().
-//                }
-
-            // Heal database so it's ready for next request.
-            healerFunctions = buildHealerFunctionsMap(databaseName);
-            databaseAuditResults.forEach((key, passedCheck) -> {
-                if (! passedCheck) {
-                    healerFunctions.get(key).run();
-                }
-            });
-        }
-
-        return new JSONObject(databaseAuditResults);
-    }
-
-    /**
-     * Builds a Map of functions that should be run if a certain check (the key) is false.
-     *
-     * @return
-     */
-    private Map<String, Runnable> buildHealerFunctionsMap(String databaseName) {
-        Map<String, Runnable> healerFunctions = new HashMap<>();
-
-        healerFunctions.put("databaseExists", () -> {
-            databaseHealerService.createDatabase();
-            databaseHealerService.createTable(databaseName);
-            databaseHealerService.insertTestData(databaseName);
-        });
-        healerFunctions.put("tableExists", () -> {
-            databaseHealerService.createTable(databaseName);
-            databaseHealerService.insertTestData(databaseName);
-        });
-        healerFunctions.put("tablesAreSame", () -> {
-            databaseHealerService.dropAllTablesExcept(databaseName, "county_spending_detail");
-        });
-        healerFunctions.put("numOfTableColumnsAreSame", () -> {
-            databaseHealerService.dropTable(databaseName);
-            databaseHealerService.createTable(databaseName);
-            databaseHealerService.insertTestData(databaseName);
-        });
-        healerFunctions.put("numOfTableRowsAreSame", () -> {
-            databaseHealerService.dropTable(databaseName);
-            databaseHealerService.createTable(databaseName);
-            databaseHealerService.insertTestData(databaseName);
-        });
-        healerFunctions.put("tableDataIsSame", () -> {
-            databaseHealerService.dropTable(databaseName);
-            databaseHealerService.createTable(databaseName);
-            databaseHealerService.insertTestData(databaseName);
-        });
-
-        return healerFunctions;
-    }
-
-    /**
-     * Wrapper method for publishing a message to SNS.  This method includes a throws clause in the signature header
-     * to call attention to the fact that the SNS client could throw various Exception child classes.
-     *
-     * @throws Exception
-     */
-//    private void publishSnsMessage() throws Exception {
-//        String snsMessage = "A query was submitted that caused a database audit result to fail. Please " +
-//                "check the S3 logs for more information.";
-//        String arn = "arn:aws:sns:us-east-1:526661363425:qb4j-sql-injection-success";
-//        PublishRequest publishRequest = new PublishRequest(arn, snsMessage);
-//        snsClient.publish(publishRequest);
-//    }
 }
