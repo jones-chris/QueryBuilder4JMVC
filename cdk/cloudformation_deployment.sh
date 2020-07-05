16eeb08e990ef79154bbd576f116bbe7fb452a45
Chris Jones <chris.jones@cfacorp.com>
2020-07-05 08:59:19 -0400

    Updated README with deployment info and deleted cdk directory after converting cdk.out CloudFormation templates to YAML

--- cdk/cloudformation_deployment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find ./cdk/cdk.out -name "*.template.json" > ./cf_stacks.txt
-
-while read line; do
-  if [ $line != "./cdk/cdk.out/CodeBuildStack.template.json" ]; then
-    aws cloudformation deploy --template-file $line --stack-name "test" --capabilities CAPABILITY_NAMED_IAM
-  fi;
-done < ./cf_stacks.txt
